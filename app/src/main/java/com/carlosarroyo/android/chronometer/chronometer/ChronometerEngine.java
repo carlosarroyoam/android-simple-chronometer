dc03cbe9bdf4306a950be04f73045f695c6d2356
Carlos Alberto Arroyo Martínez <43684710+carlosarroyoam@users.noreply.github.com>
2020-02-16 22:43:25 -0600

    Updated: Project folder structure. Added: ChronometerEngine, ChronometerNotificationBuilder classes.

--- app/src/main/java/com/carlosarroyo/android/chronometer/chronometer/ChronometerEngine.java
+++ app/src/main/java/com/carlosarroyo/android/chronometer/chronometer/ChronometerEngine.java
@@ -8,37 +8,34 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.carlosarroyo.android.chronometer.services;
+package com.carlosarroyo.android.chronometer.chronometer;
 
-import android.app.Notification;
-import android.app.PendingIntent;
-import android.app.Service;
-import android.content.Intent;
-import android.os.Binder;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.SystemClock;
-import android.util.Log;
-
-import com.carlosarroyo.android.chronometer.App;
-import com.carlosarroyo.android.chronometer.MainActivity;
-import com.carlosarroyo.android.chronometer.R;
 
 import java.util.Locale;
 
 /**
  * @author Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
  */
-public class ChronoService extends Service {
+public class ChronometerEngine {
 
-	private static final String TAG = ChronoService.class.getSimpleName();
-	private static final int NOTIFICATION_ID = 12887952;
-	private final IBinder binder = new LocalBinder();
-
-	/* Chronos variables */
+	/**
+	 * Chronometer variables
+	 */
 	private long mStartTime, mTimeMinusStart, mTimeWhenPaused, mCurrentMilliseconds;
 	private int mHours, mMinutes, mSeconds, mMilliseconds;
-	private String mFullTimeString = "00", mMillisString = "00";
+	private String mFullTimeString = "0", mMillisString = "00";
+
+	/**
+	 * Indicates if chronometer is running, variable is false when it is paused.
+	 */
+	private boolean mIsRunning = false;
+
+	/**
+	 * Indicates when chronometer has been started, no matter if it's running or paused.
+	 */
+	private boolean mIsStarted = false;
 
 	final Handler mHandler = new Handler();
 
@@ -58,54 +55,27 @@ public class ChronoService extends Service {
 			mSeconds = mSeconds % 60;
 
 			String hoursString = mHours > 0 ? String.format(Locale.US, "%d:", mHours) : "";
-			String minutesString = mMinutes > 0 ? String.format(Locale.US, "%02d:", mMinutes) : "";
-			String secondsString = String.format(Locale.US, "%02d", mSeconds);
+			String minutesString = mMinutes > 0 ? String.format(Locale.US, "%d:", mMinutes) : "";
+			String secondsString = String.format(Locale.US, "%d", mSeconds);
 
 			mFullTimeString = hoursString + minutesString + secondsString;
 			mMillisString = String.format(Locale.US, "%02d", mMilliseconds);
-
-			Log.d(TAG, "run: " + mFullTimeString);
 
 			mHandler.postDelayed(this, 0);
 		}
 	};
 
-	/**
-	 *
-	 */
-	public class LocalBinder extends Binder {
-		public ChronoService getService() {
-			return ChronoService.this;
+	public void tooglePlayPause() {
+		if (mIsRunning) {
+			mTimeWhenPaused += mTimeMinusStart;
+			mHandler.removeCallbacks(mRunnable);
+		} else {
+			mIsStarted = true;
+			mStartTime = SystemClock.uptimeMillis();
+			mHandler.postDelayed(mRunnable, 0);
 		}
-	}
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return binder;
-	}
-
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.d(TAG, "onStartCommand: ");
-		return super.onStartCommand(intent, flags, startId);
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		Log.d(TAG, "onDestroy: ");
-	}
-
-	/* Client methods */
-	public void start() {
-		createNotification();
-		mStartTime = SystemClock.uptimeMillis();
-		mHandler.postDelayed(mRunnable, 0);
-	}
-
-	public void pause() {
-		mTimeWhenPaused += mTimeMinusStart;
-		mHandler.removeCallbacks(mRunnable);
+		mIsRunning = !mIsRunning;
 	}
 
 	public void reset() {
@@ -118,11 +88,17 @@ public class ChronoService extends Service {
 		mSeconds = 0;
 		mMilliseconds = 0;
 
-		mFullTimeString = "00";
+		mFullTimeString = "0";
 		mMillisString = "00";
 
+		mIsRunning = false;
+		mIsStarted = false;
+
 		mHandler.removeCallbacks(mRunnable);
-		stopForeground(STOP_FOREGROUND_REMOVE);
+	}
+
+	public void addLap() {
+
 	}
 
 	public String getFullTime() {
@@ -133,19 +109,12 @@ public class ChronoService extends Service {
 		return mMillisString;
 	}
 
-	private void createNotification() {
-		Intent intent = new Intent(this, MainActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+	public boolean isRunning() {
+		return mIsRunning;
+	}
 
-		Notification notification = new Notification.Builder(this, App.CHRONOMETER_CHANNEL_ID)
-				.setContentTitle(getText(R.string.chronometer_notification_title))
-				.setContentText(getText(R.string.chronometer_notification_content))
-				.setSmallIcon(R.drawable.ic_launcher_foreground)
-				.setContentIntent(pendingIntent)
-				.build();
-
-		startForeground(NOTIFICATION_ID, notification);
+	public boolean isStarted() {
+		return mIsStarted;
 	}
 
 }
---
+++ app/src/main/java/com/carlosarroyo/android/chronometer/services/ChronometerService.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2020 Carlos Alberto Arroyo Martínez.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.carlosarroyo.android.chronometer.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.carlosarroyo.android.chronometer.chronometer.ChronometerEngine;
+import com.carlosarroyo.android.chronometer.notifications.ChronometerNotificationBuilder;
+
+/**
+ * @author Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
+ */
+public class ChronometerService extends Service {
+
+	private static final String TAG = ChronometerService.class.getSimpleName();
+	private final IBinder mBinder = new LocalBinder();
+	private ChronometerEngine mChronometerEngine;
+
+	/**
+	 *
+	 */
+	public class LocalBinder extends Binder {
+		public ChronometerService getService() {
+			return ChronometerService.this;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mChronometerEngine = new ChronometerEngine();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.d(TAG, "onStartCommand: ");
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mChronometerEngine = null;
+		Log.d(TAG, "onDestroy: ");
+	}
+
+	/* Client methods */
+	public void playPauseChronometer() {
+		mChronometerEngine.tooglePlayPause();
+	}
+
+	public void addLap() {
+		mChronometerEngine.addLap();
+	}
+
+	public void resetChronometer() {
+		mChronometerEngine.reset();
+		stopForeground(STOP_FOREGROUND_REMOVE);
+	}
+
+	public String getFullTime() {
+		return mChronometerEngine.getFullTime();
+	}
+
+	public String getMillisecondsTime() {
+		return mChronometerEngine.getMillisecondsTime();
+	}
+
+	public boolean isRunning() {
+		return mChronometerEngine.isRunning();
+	}
+
+	public void updateNotification() {
+		if (mChronometerEngine.isStarted()) {
+			startForeground(ChronometerNotificationBuilder.NOTIFICATION_ID, ChronometerNotificationBuilder.build(this, mChronometerEngine.getFullTime()));
+		}
+	}
+
+}
